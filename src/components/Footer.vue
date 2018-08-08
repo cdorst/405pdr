2f5ac6d54c516be0867a8d5fa90d1e8feec3debb
Christopher Dorst <chris@dorst.email>
2018-08-08 18:58:00 -0500

    Add copyright to footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,15 +1,24 @@
 <template>
-  <footer class="footer bg-light text-center">
+  <footer class="footer bg-light">
     <div class="container">
-      <a class="text-dark pr-3" href="https://facebook.com/405-PDR-901136290077736/">
-        <i class="fab fa-facebook-square fa-lg"></i>
-      </a>
-      <a class="text-dark" href="https://instagram.com/405pdr">
-        <i class="fab fa-instagram fa-lg"></i>
-      </a>
-      <a class="text-dark pl-3" href="https://www.youtube.com/channel/UCp366OzueWxyqWkDHiBatZA">
-        <i class="fab fa-youtube-square fa-lg"></i>
-      </a>
+      <div class="row">
+        <div class="col text-center social">
+          <a class="text-dark pr-3" href="https://facebook.com/405-PDR-901136290077736/">
+            <i class="fab fa-facebook-square fa-lg"></i>
+          </a>
+          <a class="text-dark" href="https://instagram.com/405pdr">
+            <i class="fab fa-instagram fa-lg"></i>
+          </a>
+          <a class="text-dark pl-3" href="https://www.youtube.com/channel/UCp366OzueWxyqWkDHiBatZA">
+            <i class="fab fa-youtube-square fa-lg"></i>
+          </a>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col text-center">
+          <span>Copyright &copy; 2018 405 PDR LLC.</span>
+        </div>
+      </div>
     </div>
   </footer>
 </template>
@@ -27,7 +36,9 @@ export default {
   bottom: 0;
   width: 100%;
   /* Set the fixed height of the footer here */
-  height: 60px;
-  line-height: 60px; /* Vertically center the text there */
+  height: 90px;
+}
+.social {
+  line-height: 60px;
 }
 </style>
